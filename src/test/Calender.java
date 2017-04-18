dd9bd2c8258396baa7fb5bb8c28f3418a93bd648
SeungHeon Song <58469214as@naver.com>
2017-04-18 18:59:37 +0900

    First Commit

---
+++ src/test/Calender.java
@@ -0,0 +1,80 @@
+package test;
+
+import java.util.Scanner;
+
+public class Calender {
+
+	public static void main(String[] args) {
+		   System.out.println("input year ");
+	        Scanner scanner1 = new Scanner(System.in);
+	        int year = scanner1.nextInt();
+
+	        System.out.println("input month");
+	        Scanner scanner2 = new Scanner(System.in);
+	        int month = scanner2.nextInt();
+
+	        System.out.println("input day");
+	        Scanner scanner3 = new Scanner(System.in);
+	        int day = scanner1.nextInt();
+
+	        int[] max_value_month = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	        int sum_month_temp = 0;
+	        int total_days = 0;
+
+	        total_days += (year - 1) + 365;
+	        total_days += year / 4 - year / 100 + year / 400;
+
+	        int four_multiple = year % 4;
+	        int hundred_multiple = year % 100;
+
+
+	        if (four_multiple == 0 && hundred_multiple == 0) {
+	            if (month == 1 || month == 2) {
+	                total_days--;
+	                max_value_month[1] = 29;
+	            }
+	        } else if (four_multiple == 0 && hundred_multiple != 0) {
+	            if (month == 1 || month == 2) {
+	                total_days--;
+	                max_value_month[1] = 29;
+	            }
+	        } else {
+	        }
+
+
+	        if (month ==1 ){
+	            sum_month_temp = 0;
+	        }
+	        else{
+	            for(int i=0; i< month -1; i++){
+	                sum_month_temp += max_value_month[i];
+
+	                total_days += sum_month_temp = day;
+	            }
+	        }
+
+
+	        System.out.println("  "+"sun   "+"mon   "+"Tue   "+"Wnd   "+"Thr   "+"Fri   "+"Sat    " );
+
+	        int check_day_of_the_week = (total_days+2) % 7;
+
+	        for(int i=0;i<check_day_of_the_week; i++){
+	            System.out.printf("     ");
+	        }
+
+	        for(int j = 1;j<=max_value_month[month-1];j++){
+	            if(day == j){
+	                System.out.printf("%4d"+"*",j);
+	            }
+	            else{
+	                System.out.printf("%4d"+" ",j);
+	            }
+	            if((j+check_day_of_the_week)%7 == 0 ) System.out.println();
+	        }
+
+
+
+
+	}
+
+}
